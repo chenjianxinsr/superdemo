e724621629ccb0032a810888c858f40e24ea0111
chenjianxin <chenjx@yunrong.cn>
2019-11-25 14:29:04 +0800

    test upload git

---
+++ src/main/java/com/asin/superdemo/SuperdemoApplication.java
@@ -0,0 +1,13 @@
+package com.asin.superdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperdemoApplication.class, args);
+    }
+
+}
